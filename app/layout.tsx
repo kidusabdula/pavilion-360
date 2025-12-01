96d34f00823332cc4abf9955fc0d0ba75b36f330
Naol22 <Naolgirma21@gmail.com>
2025-12-01 13:33:52 +0300

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,24 +29,7 @@ export const metadata: Metadata = {
     "wedding production",
     "Indianapolis",
   ],
-  generator: "v0.app",
-  icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
-  },
+
 }
 
 // Import header and footer components
