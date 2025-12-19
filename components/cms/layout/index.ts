028a525aec4f68134e3343b40ad1362cd48f6fe4
Kidus Abdula <Kidus489@gmail.com>
2025-12-19 13:15:02 +0300

    CMS: Layout Barrel Export

---
+++ components/cms/layout/index.ts
@@ -0,0 +1,5 @@
+// components/cms/layout/index.ts
+export { CMSShell } from './cms-shell';
+export { CMSSidebar } from './cms-sidebar';
+export { CMSHeader } from './cms-header';
+export { CMSBreadcrumb } from './cms-breadcrumb';
