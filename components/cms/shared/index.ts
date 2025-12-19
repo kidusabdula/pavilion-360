86da494b29e965c31b8ad194e774e1f542ac5720
Kidus Abdula <Kidus489@gmail.com>
2025-12-19 14:17:28 +0300

    CMS: Barrel Export

---
+++ components/cms/shared/index.ts
@@ -0,0 +1,7 @@
+// components/cms/shared/index.ts
+export { PageHeader } from './page-header';
+export { EmptyState } from './empty-state';
+export { TableSkeleton, FormSkeleton, DetailSkeleton, CardSkeleton } from './loading-skeleton';
+export { ConfirmDialog } from './confirm-dialog';
+export { StatusBadge } from './status-badge';
+export { DataTable, TableThumbnail } from './data-table';
